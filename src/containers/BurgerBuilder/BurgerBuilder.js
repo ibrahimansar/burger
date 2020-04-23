a731d32a5c089d9be9fd2cd0600260981a35bbeb
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2020-04-23 17:34:22 +0530

    .

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import Aux from '../../hoc/Auxiliary';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
+import Spinner from '../../components/UI/Spinner/Spinner'
+import axios from '../../axios-orders';
 import OrderSummary from '../../components/Burger/OrderedSummary/OrderedSummary';
 
 const INGREDIENT_PRICES ={
@@ -23,7 +25,8 @@ class BurgerBuilder extends Component {
         },
         totalPrice : 4,
         purchasable: false,
-        purchasing: false
+        purchasing: false,
+        loading: false
     }
 
     updatePurchaseState (ingredients) {
@@ -60,7 +63,30 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler =() =>{
-        alert('you continue!');
+       // alert('you continue!');
+       this.setState({loading: true});   
+       const order ={        
+           ingredients : this.state.ingredients,
+           price : this.state.price,
+           customer :{
+                name: 'ansari',
+                address : {
+                    street : 'New Bazar Street',
+                    pincode : '628204',
+                    country : 'india'
+                },
+                email : 'mdansariibrahim1@gmail.com',
+            },
+            deliveryMethod : 'fastest'
+       }    
+       axios.post('/order.json', order)
+       .then(response =>{ 
+        this.setState({loading: false, purchasing: false});
+        })
+
+       .catch(error => {
+           this.setState({loading: false, purchasing: false});
+       });
     }
     removeIngredientHandler =(type) =>{
 
@@ -88,17 +114,21 @@ class BurgerBuilder extends Component {
             disabledInfo[key]= disabledInfo[key]<=0 
         }    
 
+        let orderSummary = <OrderSummary
+        ingredients={this.state.ingredients}
+        price={this.state.totalPrice}
+        purchaseCancelled={this.purchaseCancelHandler}
+        purchaseContinued={this.purchaseContinueHandler} />;
+    if ( this.state.loading ) {
+        orderSummary = <Spinner />;
+    }
+
+
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler} >
-                    <OrderSummary
-                    ingredients={this.state.ingredients} 
-                    purchaseCanceled={this.purchaseCancelHandler}
-                    purchaseContinued ={this.purchaseContinueHandler}
-                    price= {this.state.totalPrice}/>
+                     {orderSummary}
                 </Modal>
-
-
                 
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls 
