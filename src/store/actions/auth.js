51ef0021a4b11f976d92dd208954638d379a1823
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2020-06-09 21:32:40 +0530

    .

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,6 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
 
 export const authStart = () => {
     return {
@@ -20,8 +22,27 @@ export const authFail = (error) => {
     };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAcJwzjKr_bdcxIR8uD-eO3zVUQuzN_yAU';
+        if (!isSignup){
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAcJwzjKr_bdcxIR8uD-eO3zVUQuzN_yAU';
+        }
+        axios.post(url, authData)
+        .then(response => {
+            console.log(response);
+            dispatch(authSuccess(response.data))            
+        })
+        .catch(err => {
+            console.log(err);
+            dispatch(authFail(err))
+        })
+
     };
 };
