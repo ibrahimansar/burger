1788fa74f1ce678dfe44fa24ef5231b948a84a69
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2020-06-14 20:18:56 +0530

    Burger over

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import authReducer from './store/reducers/auth';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
-import orderReducer from './store/reducers/order'
+import orderReducer from './store/reducers/order';
+import authReducer from './store/reducers/auth';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = process.env.NODE_ENV === 'development' ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : null || compose;
 
-const rootReducer = combineReducers ( {
+const rootReducer = combineReducers({
     burgerBuilder: burgerBuilderReducer,
     order: orderReducer,
     auth: authReducer
-})
+});
 
 const store = createStore(rootReducer, composeEnhancers(
     applyMiddleware(thunk)
