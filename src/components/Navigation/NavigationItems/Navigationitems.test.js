69ed76598f332b3b6576e46ff24c348567c4e2c4
Mohamed Ibrahim Ansari <54184745+ibrahimansar@users.noreply.github.com>
2020-06-15 23:40:58 +0530

    TEST

---
+++ src/components/Navigation/NavigationItems/Navigationitems.test.js
@@ -0,0 +1,12 @@
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import NavigationItems from './NavigationItems'
+import NavigationItem from './NavigationItem/NavigationItem';
+configure( {adapter: new Adapter()} );
+
+describe('<Navigation />', () => {
+    it('should render two <NavigationItem /> elements if not authentivated', ()=> {
+        const wrapper = shallow(<NavigationItems/>);
+        expect(wrapper.find(NavigationItem)).tohaveLength(2);
+     });    
+});
